56956e53f796c24aabcf4b7c6638200a8fce1d90
guzalmusurmonova <burxonovsherzod0025@gmail.com>
2023-05-01 05:00:32 -0700

    07

--- find07_min.py
+++ find07_min.py
@@ -6,4 +6,12 @@ def find_min(data):
     returns: minimum number in the list
     """
 
-    return 0
+    i=0
+    n=0
+    a=data[0]
+    while n<len(data):
+        if a>data[n]:
+            a=data[n]
+        n=n+1
+    return a
+print(find_min([1, 2, -3, 4, 5]))
