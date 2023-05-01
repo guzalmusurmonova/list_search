d3d3d75bc192aff2240e5cc9b997f85cd70ad2d9
guzalmusurmonova <burxonovsherzod0025@gmail.com>
2023-05-01 03:30:35 -0700

    01

--- find01_max.py
+++ find01_max.py
@@ -4,6 +4,14 @@ def find_max(data):
     args:
         data: list of numbers
     returns: maximum number in the list
-    """
-    return 0
+    """ 
+    i=0
+    n=0
+    a=data[0]
+    while n<len(data):
+        if a<data[n]:
+            a=data[n]
+        n=n+1
+    return a
+print(find_max([1, 2, 3, 4, 5]))
     
