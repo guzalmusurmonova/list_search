4219202c204324937d84b537cacad9ee2608cf3b
guzalmusurmonova <burxonovsherzod0025@gmail.com>
2023-05-01 04:57:46 -0700

    05

--- find05_max_even.py
+++ find05_max_even.py
@@ -4,5 +4,16 @@ def find_max_even(data):
     args:
         data: list of numbers
     returns: maximum even number in the list
-    """
-    return 0
+    """ 
+    i=0
+    n=0
+    a=data[0]
+    while n<len(data):
+        if a<data[n]:
+            a=data[n] 
+        if a%2!=0:
+            a=data[n]
+        n=n+1
+    return a
+print(find_max_even([1, 4, 3, 8, 5]))
+    
